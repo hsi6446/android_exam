474746680e9b72edf4b2be789752c45bf3938d35
Suin.heo <hsi6446@gmail.com>
2015-09-04 13:24:24 +0900

    practice adjust complete

--- app/src/main/java/com/example/android/android_exam/Mission03/MenuActivity.java
+++ app/src/main/java/com/example/android/android_exam/Mission03/MenuActivity.java
@@ -1,60 +1,59 @@
+
 package com.example.android.android_exam.Mission03;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 
 import com.example.android.android_exam.R;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
     public static final int INTENT2 = 1002;
     public static final int INTENT3 = 1003;
     public static final int INTENT4 = 1004;
-
-    private Button mCustomerMenu;
-    private Button mSalesMenu;
-    private Button mProductMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        mCustomerMenu = (Button)findViewById(R.id.customer_menu);
-        mSalesMenu = (Button)findViewById(R.id.sales_menu);
-        mProductMenu = (Button)findViewById(R.id.product_menu);
+        findViewById(R.id.customer_menu).setOnClickListener(this);
+        findViewById(R.id.sales_menu).setOnClickListener(this);
+        findViewById(R.id.product_menu).setOnClickListener(this);
 
 
-        mCustomerMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent2 = new Intent(getApplicationContext(), customerActivity.class);
-                startActivityForResult(intent2, INTENT2);
-            }
-        });
-
-        mSalesMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent3 = new Intent(getApplicationContext(), SalesMainActivity.class);
-                startActivityForResult(intent3, INTENT3);
-            }
-        });
-
-        mProductMenu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent4 = new Intent(getApplication(), ProductActivity.class);
-                startActivityForResult(intent4, INTENT4);
-            }
-        });
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.customer_menu:
+                customerClick();
+                break;
 
+            case R.id.sales_menu:
+                salesClick();
+                break;
+            case R.id.product_menu:
+                productClick();
+                break;
+        }
+    }
 
+    private void productClick() {
+        Intent intent4 = new Intent(getApplication(), ProductActivity.class);
+         startActivityForResult(intent4, INTENT4);
+    }
 
+    private void salesClick() {
+        Intent intent2 = new Intent(getApplicationContext(), customerActivity.class);
+        startActivityForResult(intent2, INTENT2);
+    }
 
+    private void customerClick() {
+        Intent intent2 = new Intent(getApplicationContext(), customerActivity.class);
+        startActivityForResult(intent2, INTENT2);
+    }
 }
