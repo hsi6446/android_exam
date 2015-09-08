1fc637713a9dafe89a238a34d15c9c45284ee6a5
Suin.heo <hsi6446@gmail.com>
2015-09-08 11:38:28 +0900

    basic style of View holder

--- app/src/main/java/com/example/android/android_exam/Calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/android/android_exam/Calendar/CalendarAdapter.java
@@ -1,9 +1,13 @@
 package com.example.android.android_exam.Calendar;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.android.android_exam.R;
 
 import java.util.Calendar;
 import java.util.List;
@@ -17,7 +21,7 @@ public class CalendarAdapter extends BaseAdapter {
     private Context mContext;
 
     public CalendarAdapter(Context context, List<Calendar> list) {
-        this.mList = list;
+        mList = list;
         mContext = context;
     }
 
@@ -36,8 +40,33 @@ public class CalendarAdapter extends BaseAdapter {
         return position;
     }
 
+
+    //아이템이 화면에 보일 때 호출 됨.
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        ViewHolder holder;
+
+        //레이아웃을 완성
+        if (convertView == null) {
+            holder = new ViewHolder();
+            //처음 로드
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_calendar, parent);
+
+            holder.dateTextView = (TextView)convertView.findViewById(R.id.date_text_view);
+
+            convertView.setTag(holder);
+        } else  {
+            //재사용
+            holder = (ViewHolder)convertView.getTag();
+
+        }
+
+        //데이터를 레이아웃에 설정
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView dateTextView;
     }
 }
