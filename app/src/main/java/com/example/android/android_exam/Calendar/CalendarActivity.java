b5778b5e5124579ba0fe3db7f3e1e74b54830d39
Suin.heo <hsi6446@gmail.com>
2015-09-08 16:38:39 +0900

    calendar complete
    Grid view & Adapter & etc

--- app/src/main/java/com/example/android/android_exam/Calendar/CalendarActivity.java
+++ app/src/main/java/com/example/android/android_exam/Calendar/CalendarActivity.java
@@ -2,55 +2,62 @@ package com.example.android.android_exam.Calendar;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.android.android_exam.R;
 
-import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.GregorianCalendar;
 import java.util.List;
 
-public class CalendarActivity extends AppCompatActivity {
+public class CalendarActivity extends AppCompatActivity implements View.OnClickListener {
 
     private List<Calendar> mList;
     private CalendarAdapter mCalendarAdaptor;
     private CalendarView mCalendarView;
+    private TextView mTitleTextView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
 
-        mList = new ArrayList<>();
+        // 버튼 이벤트 연결
+        findViewById(R.id.prev_btn).setOnClickListener(this);
+        findViewById(R.id.next_btn).setOnClickListener(this);
+        mTitleTextView = (TextView)findViewById(R.id.title_text_view);
 
-        // 오늘 날짜
-        Calendar calendar = GregorianCalendar.getInstance();
 
-        int year = calendar.get(calendar.YEAR);
-        int month = calendar.get(calendar.MONTH) + 1;
-
-        // 마지막 날
-        int lastDay = calendar.getActualMaximum(Calendar.DATE);
-
-        // 이 달의 첫 번째 날
-        calendar.set(Calendar.DAY_OF_MONTH, 1);
-        int firstday = calendar.get(Calendar.DAY_OF_WEEK);
-
-        for(int i = 1; i < firstday; i++){
-            mList.add(null);
-        }
-
-        // 이번 달 달력 데이터
-        for(int i = 1; i<= lastDay; i++) {
-            mList.add(new GregorianCalendar(year, month, i));
-        }
 
         // 어뎁터 준비
-        mCalendarAdaptor = new CalendarAdapter(CalendarActivity.this, mList);
+        mCalendarAdaptor = new CalendarAdapter(this);
 
         // view 에 어뎁터를 설정
         mCalendarView = (CalendarView)findViewById(R.id.calendar);
         mCalendarView.setAdapter(mCalendarAdaptor);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.prev_btn:
+                mCalendarAdaptor.prevMonth();
+                updateTitle();
+                break;
+            case R.id.next_btn:
+                mCalendarAdaptor.nextMonth();
+                updateTitle();
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void updateTitle() {
+        int year = mCalendarAdaptor.getCalendar().get(Calendar.YEAR);
+        int month = mCalendarAdaptor.getCalendar().get(Calendar.MONTH) + 1;
+        mTitleTextView.setText(year + "년 " + month + "월");
+    }
 }
--- app/src/main/java/com/example/android/android_exam/Calendar/CalendarAdapter.java
+++ app/src/main/java/com/example/android/android_exam/Calendar/CalendarAdapter.java
@@ -1,6 +1,8 @@
+
 package com.example.android.android_exam.Calendar;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +11,9 @@ import android.widget.TextView;
 
 import com.example.android.android_exam.R;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 /**
@@ -19,10 +23,62 @@ public class CalendarAdapter extends BaseAdapter {
 
     private List<Calendar> mList;
     private Context mContext;
+    private Calendar mCalendar;
 
-    public CalendarAdapter(Context context, List<Calendar> list) {
-        mList = list;
+    public CalendarAdapter(Context context) {
         mContext = context;
+
+        // 오늘 날짜
+        mCalendar = GregorianCalendar.getInstance();
+        createCalendar(mCalendar);
+
+    }
+
+    private void createCalendar(Calendar calendar) {
+        mList = new ArrayList<>();
+
+        int year = mCalendar.get(Calendar.YEAR);
+        int month = mCalendar.get(Calendar.MONTH);
+
+        // 마지막 날
+        int lastDay = mCalendar.getActualMaximum(Calendar.DATE);
+
+        // 이 달의 첫 번째 날
+        mCalendar.set(Calendar.DAY_OF_MONTH, 1);
+        int firstday = mCalendar.get(Calendar.DAY_OF_WEEK);
+
+        // 공백
+        for (int i = 1; i < firstday; i++) {
+            mList.add(null);
+        }
+
+        // 이번 달 달력 데이터
+        for (int i = 1; i <= lastDay; i++) {
+            mList.add(new GregorianCalendar(year, month, i));
+        }
+    }
+
+    public void prevMonth() {
+
+        changeMonth(-1);
+    }
+
+    public void nextMonth() {
+        changeMonth(1);
+
+    }
+
+    public Calendar getCalendar() {
+        return mCalendar;
+    }
+
+    private void changeMonth(int month) {
+        // 다음달로 설정
+        mCalendar.add(Calendar.MONTH, month);
+        createCalendar(mCalendar);
+
+        // 어뎁터에 바뀐 데이터를 반영하도록 할려 줌
+        notifyDataSetChanged();
     }
 
     @Override
@@ -40,33 +96,41 @@ public class CalendarAdapter extends BaseAdapter {
         return position;
     }
 
-
-    //아이템이 화면에 보일 때 호출 됨.
+    // 아이템이 화면에 보일 때 호출 됨.
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         ViewHolder holder;
 
-        //레이아웃을 완성하고 뷰홀더와 연결
+        // 레이아웃을 완성하고 뷰홀더와 연결
         // findViewById를 한번만 사용하기 위해.
         if (convertView == null) {
             holder = new ViewHolder();
-            //처음 로드
-            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_calendar, parent, false);
+            // 처음 로드
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.item_calendar, parent,
+                    false);
 
-            holder.dateTextView = (TextView)convertView.findViewById(R.id.date_text_view);
+            holder.dateTextView = (TextView) convertView.findViewById(R.id.date_text_view);
 
             convertView.setTag(holder);
-        } else  {
-            //재사용
-            holder = (ViewHolder)convertView.getTag();
+        } else {
+            // 재사용
+            holder = (ViewHolder) convertView.getTag();
 
         }
 
-        //데이터를 레이아웃에 설정
+        // 데이터를 레이아웃에 설정
         Calendar calendar = mList.get(position);
-        if(calendar != null) {
+        if (calendar != null) {
             holder.dateTextView.setText("" + calendar.get(calendar.DATE));
-        }else {
+
+            if(position % 7 == 0) {
+                holder.dateTextView.setTextColor(Color.RED);
+            }else if((position + 1) % 7 == 0) {
+                holder.dateTextView.setTextColor(Color.BLUE);
+            }else{
+                holder.dateTextView.setTextColor(Color.BLACK);
+            }
+        } else {
             holder.dateTextView.setText("");
         }
         return convertView;
