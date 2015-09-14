f71c90b1df74d01c62cb9fbbc5687701d12d8dac
Suin.heo <hsi6446@gmail.com>
2015-09-14 11:23:22 +0900

    AsyncTask example

--- app/src/main/java/com/example/android/android_exam/thread/ThreadActivity.java
+++ app/src/main/java/com/example/android/android_exam/thread/ThreadActivity.java
@@ -2,13 +2,16 @@
 package com.example.android.android_exam.thread;
 
 import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.android_exam.R;
@@ -20,6 +23,8 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
 
     private TextView mNumber1;
     private TextView mNumber2;
+
+    private ProgressBar mProgressBar;
 
 
     @Override
@@ -33,8 +38,11 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
         mNumber1 = (TextView)findViewById(R.id.tv_number1);
         mNumber2 = (TextView)findViewById(R.id.tv_number2);
 
+        mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
+
         mbtnThread1.setOnClickListener(this);
         mbtnThread2.setOnClickListener(this);
+
     }
 
 
@@ -44,8 +52,16 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.btn_thread1 :
                 progressDialogExam();
 
+                //굉장히 오래 걸리는 처리(10초)
+                // for(...)
+
+
+                //완료되었습니다.
+
                 break;
             case R.id.btn_thread2 :
+                new DownloadTask().execute();
+
                 break;
         }
 
@@ -79,7 +95,7 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
     }
 
 
-    // 백그라운드 처리는 바로바로 보이지만, UI 변경은 스레드 종요 시 마지막 결과만 보여진다.
+    // 백그라운드 처리는 바로바로 보이지만, UI 변경은 스레드 종료 시 마지막 결과만 보여진다.
     private void runOnUIThreadExam() {
         // UI Thread 로 동작하게 해줌. Activity 가 제공함(액티비티 안에서만 사용 가능)
         runOnUiThread(new Runnable() {
@@ -164,5 +180,62 @@ public class ThreadActivity extends AppCompatActivity implements View.OnClickLis
             }
         });
         thread.start();
+    }
+
+    private class DownloadTask extends AsyncTask<Void, Integer, Void> {
+        private AlertDialog.Builder mmBuilder;
+
+
+        // UI Thread
+        // doInBackground 전에 호출 됨.
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mmBuilder = new AlertDialog.Builder(ThreadActivity.this);
+            mmBuilder.setMessage("다운로드 완료 되었습니다.");
+            mmBuilder.setNegativeButton("닫기", null);
+
+            mProgressBar.setProgress(0);
+        }
+
+        @Override
+        // Background Thread 처리하는 부분.
+        protected Void doInBackground(Void... params) {
+            // 다운로드 처리
+            for (int i = 0; i < 100; i++) {
+                // 0.2초 쉬고
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                // onProgressUpdate 를 호출
+                publishProgress(i + 1);
+            }
+            return null;
+        }
+
+        // UI Thread
+        // doInBackground 에서 publishProgress 로 호출하면 호출 됨.
+        // 직접 호출하면 프로그램이 죽으므로, 간접적으로 호출 됨.
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+
+            mProgressBar.setProgress(values[0]);
+            mNumber2.setText(values[0] + "%");
+        }
+
+        // UI Thread
+        // doInBackground 가 수행된 후에 호출 됨.
+        // doInBackground 에서 리턴 된 값이 파라미터로 넘어옴.
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+
+            mmBuilder.show();
+        }
     }
 }
