4754b2cd671cdb3e7719049a6ec591eef485975e
Suin.heo <hsi6446@gmail.com>
2015-09-16 10:45:51 +0900

    backup

--- app/src/main/java/com/example/android/android_exam/viewPager/ScreenSlideActivity.java
+++ app/src/main/java/com/example/android/android_exam/viewPager/ScreenSlideActivity.java
@@ -1,6 +1,7 @@
 package com.example.android.android_exam.viewPager;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -19,6 +20,8 @@ import java.util.List;
 public class ScreenSlideActivity extends AppCompatActivity {
     private ViewPager mViewPager;
 
+    private TabLayout mTabLayout;
+
     private List<Fragment> mFragmentList;
 
     @Override
@@ -27,14 +30,19 @@ public class ScreenSlideActivity extends AppCompatActivity {
         setContentView(R.layout.activity_screen_slide);
         mViewPager = (ViewPager)findViewById(R.id.viewPager);
 
+        mTabLayout = (TabLayout)findViewById(R.id.tap_layout);
+
+        // 짝퉁 데이터
         mFragmentList = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             mFragmentList.add(new ColorFragment());
+            mTabLayout.addTab(mTabLayout.newTab().setText("Tab" + (i +1)));
         }
 
         ScreenSlidePagerAdapter adapter = new ScreenSlidePagerAdapter(getSupportFragmentManager(), mFragmentList);
 
         mViewPager.setAdapter(adapter);
+        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
     }
 
     // FragmentStatePagerAdapter 는 메모리 관리가 필요할 때 사용.
