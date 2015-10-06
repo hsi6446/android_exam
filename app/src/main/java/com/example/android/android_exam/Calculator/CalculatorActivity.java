7049b0042303880568f378abd9d479170b7cff9e
Suin.heo <hsi6446@gmail.com>
2015-10-06 11:53:37 +0900

    calculator ing...

--- app/src/main/java/com/example/android/android_exam/Calculator/CalculatorActivity.java
+++ app/src/main/java/com/example/android/android_exam/Calculator/CalculatorActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.android_exam.R;
 
@@ -14,9 +15,16 @@ import com.example.android.android_exam.R;
 public class CalculatorActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mConsole;
-    private int mResult = 0;
+    private TextView mMiddle;
+
+    private int mResult = 0;        // 결과창에 표시될 데이터
     private int mSave1 = 0;         // 이전 연산자
     private int mSave2 = 0;         // 다음 연산자
+
+    private int timesResult = 0;
+    private int divideResult = 0;
+    private int plusResult = 0;
+    private int minusResult = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,7 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         setContentView(R.layout.activity_calculater);
 
         mConsole = (TextView) findViewById(R.id.tv_console);
+        mMiddle = (TextView) findViewById(R.id.tv_middle);
 
         findViewById(R.id.btn_times).setOnClickListener(this);
         findViewById(R.id.btn_divide).setOnClickListener(this);
@@ -46,89 +55,115 @@ public class CalculatorActivity extends AppCompatActivity implements View.OnClic
         findViewById(R.id.btn_all_cancle).setOnClickListener(this);
         findViewById(R.id.btn_erase).setOnClickListener(this);
 
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_equals:
+                mConsole.setText(Integer.toString(mResult));
                 break;
             case R.id.btn_point:
+                Toast.makeText(CalculatorActivity.this, "" + mSave1, Toast.LENGTH_SHORT).show();
                 break;
             case R.id.btn_erase:
+                if (!mMiddle.getText().toString().equals("")) {
+                    String e = mMiddle.getText().toString().substring(0, mMiddle.getText().toString().length() - 1);
+                    mMiddle.setText(e);
+                } else {
+                    mMiddle.setText("");
+                }
                 break;
             case R.id.btn_all_cancle:
-                mConsole.setText(0);
+                mConsole.setText("");
+                mMiddle.setText("");
                 mResult = 0;
                 mSave1 = 0;
                 break;
 
             case R.id.btn_times:
-                onTimes();
+                mSave2 = Integer.valueOf(mMiddle.getText().toString().trim());
+                onTimes(mSave2);
+                mMiddle.setText("");
                 break;
             case R.id.btn_divide:
-                onDivide();
+                mSave2 = Integer.valueOf(mMiddle.getText().toString().trim());
+                onDivide(mSave2);
+                mMiddle.setText("");
                 break;
             case R.id.btn_plus:
-                onPlus();
+                mSave2 = Integer.valueOf(mMiddle.getText().toString().trim());
+                onPlus(mSave2);
+                mMiddle.setText("");
                 break;
             case R.id.btn_minus:
-                onMinus();
+                mSave2 = Integer.valueOf(mMiddle.getText().toString().trim());
+                onMinus(mSave2);
+                mMiddle.setText("");
                 break;
 
             case R.id.btn_one:
-                mSave1 += 1;
+                mMiddle.setText(mMiddle.getText().toString() + 1);
                 break;
             case R.id.btn_two:
-                mSave1 += 2;
+                mMiddle.setText(mMiddle.getText().toString() + 2);
                 break;
             case R.id.btn_three:
-                mSave1 += 3;
+                mMiddle.setText(mMiddle.getText().toString() + 3);
                 break;
             case R.id.btn_four:
-                mSave1 += 4;
+                mMiddle.setText(mMiddle.getText().toString() + 4);
                 break;
             case R.id.btn_five:
-                mSave1 += 5;
+                mMiddle.setText(mMiddle.getText().toString() + 5);
                 break;
             case R.id.btn_six:
-                mSave1 += 6;
+                mMiddle.setText(mMiddle.getText().toString() + 6);
                 break;
             case R.id.btn_seven:
-                mSave1 += 7;
+                mMiddle.setText(mMiddle.getText().toString() + 7);
                 break;
             case R.id.btn_eight:
-                mSave1 += 8;
+                mMiddle.setText(mMiddle.getText().toString() + 8);
                 break;
             case R.id.btn_nine:
-                mSave1 += 9;
+                mMiddle.setText(mMiddle.getText().toString() + 9);
                 break;
             case R.id.btn_zero:
-                mSave1 += 0;
+                mMiddle.setText(mMiddle.getText().toString() + 0);
                 break;
 
         }
 
     }
 
-    private int onTimes() {
+    private int onTimes(int mSave2) {
 
-        return 0;
+        mResult = (mSave1 = mSave1 * mSave2);
+
+        return mResult;
     }
 
-    private int onDivide() {
+    private int onDivide(int mSave2) {
 
-        return 0;
+        mResult = (mSave1 = mSave2 / Integer.valueOf(mMiddle.getText().toString().trim()));
+
+        return mResult;
     }
 
-    private int onPlus() {
+    private int onPlus(int mSave2) {
 
-        return 0;
+        mResult = (mSave1 = mSave2 + Integer.valueOf(mMiddle.getText().toString().trim()));
+
+        return mResult;
     }
 
-    private int onMinus() {
+    private int onMinus(int mSave2) {
 
-        return 0;
+        mResult = (mSave1 = mSave1 - mSave2);
+
+        return mResult;
     }
 
 
